c5d0aaa2ef1b17808a0bc8def0ae52a4510dc21c
Ashok Dasari <ashokd9@gmail.com>
2020-03-21 17:40:37 -0500

    1st Version of the algorithms.

---
+++ sorting-algorithms/sorting-algorithms/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace sorting_algorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the elements to be sorted seperated by space");
+            string arraytobeSorted = Console.ReadLine();
+            var origArr = Array.ConvertAll((arraytobeSorted.Split(" ", StringSplitOptions.RemoveEmptyEntries)), s => int.Parse(s));
+
+            Console.WriteLine("Bubble Sorted");
+            var bubbleSorted = (int[])origArr.Clone();
+            SortingAlgorithms.BubbleSort(bubbleSorted);
+            PrintArray(bubbleSorted);
+
+            Console.WriteLine("\nInsertion Sorted");
+            var insertionSorted = (int[])origArr.Clone();
+            SortingAlgorithms.InsertionSort(insertionSorted);
+            PrintArray(insertionSorted);
+
+            Console.WriteLine("\nSelection Sorted");
+            var selectionSorted = (int[])origArr.Clone();
+            SortingAlgorithms.SelectionSort(selectionSorted);
+            PrintArray(selectionSorted);
+
+            Console.WriteLine("\nMergeSort Sorted");
+            var mergeSorted = (int[])origArr.Clone();
+            SortingAlgorithms.MergeSort(mergeSorted, 0, origArr.Length - 1);
+            PrintArray(mergeSorted);
+
+            Console.Read();
+        }
+        private static void PrintArray(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+        }
+    }
+}
---
+++ sorting-algorithms/sorting-algorithms/SortingAlgorithms.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace sorting_algorithms
+{
+    public class SortingAlgorithms
+    {
+        public static void BubbleSort(int[] elements)
+        {
+            for (int i = 0; i < elements.Length; i++)
+            {
+                for (int j = 0; j < elements.Length - i - 1; j++)
+                {
+                    int temp;
+                    if (elements[j] > elements[j + 1])
+                    {
+                        temp = elements[j];
+                        elements[j] = elements[j + 1];
+                        elements[j + 1] = temp;
+                    }
+                }
+            }
+        }
+
+        public static void InsertionSort(int[] elements)
+        {
+            for (int i = 1; i < elements.Length; i++)
+            {
+                int key = elements[i];
+                int j = i - 1;
+
+                while (j >= 0 && key < elements[j])
+                {
+                    elements[j + 1] = elements[j];
+                    j--;
+                }
+                elements[j + 1] = key;
+            }
+        }
+
+        public static void SelectionSort(int[] elements)
+        {
+            for (int i = 0; i < elements.Length - 1; i++)
+            {
+                int min_idx = i;
+                for (int j = i + 1; j < elements.Length; j++)
+                {
+                    if (elements[j] < elements[min_idx])
+                    {
+                        min_idx = j;
+                    }
+                }
+                var temp = elements[i];
+                elements[i] = elements[min_idx];
+                elements[min_idx] = temp;
+            }
+        }
+
+        public static void MergeSort(int[] arr, int l, int r)
+        {
+            if (l < r)
+            {
+                int m = (l + r) / 2;
+                MergeSort(arr, l, m);
+                MergeSort(arr, m + 1, r);
+                Merge(arr, l, m, r);
+            }
+        }
+
+        private static void Merge(int[] arr, int l, int m, int r)
+        {
+            int n1 = m - l + 1;
+            int n2 = r - m;
+
+            var L = new int[n1];
+            var R = new int[n2];
+            for (int idx = 0; idx < n1; ++idx)
+            {
+                L[idx] = arr[l + idx];
+            }
+
+            for (int idx = 0; idx < n2; ++idx)
+            {
+                R[idx] = arr[m + idx + 1];
+            }
+
+            int i = 0, j = 0;
+            int k = l;
+            while (i < n1 && j < n2)
+            {
+                if (L[i] < R[j])
+                {
+                    arr[k] = L[i];
+                    i++;
+                }
+                else
+                {
+                    arr[k] = R[j];
+                    j++;
+                }
+                k++;
+            }
+            while (i < n1)
+            {
+                arr[k] = L[i];
+                i++;
+                k++;
+            }
+            while (j < n2)
+            {
+                arr[k] = R[j];
+                j++;
+                k++;
+            }
+        }
+    }
+}
